02d8e1cd66020789bf6439f799ef370901336345
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-03-19 00:57:55 +0200

    upgrade dependencies, config fix to specs

--- specs/test-authentication.js
+++ specs/test-authentication.js
@@ -6,7 +6,7 @@ let auth = require('../lib');
 describe('Google authentication', () => {
   describe('Test Google authentication', () => {
     it('Test signin', () => {
-      let providerConfig = config.getConfig('microsoft');
+      let providerConfig = config('microsoft');
       auth.signin(providerConfig, {}, (err, data) => {
         expect(err).to.be.null;
         expect(data.url).to.equal('https://login.live.com/oauth20_authorize.srf?client_id=fb-microsoft-id&redirect_uri=https://api-id.execute-api.eu-west-1.amazonaws.com/dev/callback/microsoft&scope=wl.basic&response_type=code');
