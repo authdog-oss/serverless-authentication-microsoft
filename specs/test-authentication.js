b85f48dd63b47db1a622a32555e7d05b5057a866
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-04-04 00:27:59 +0300

    refactored to extend default provider

--- specs/test-authentication.js
+++ specs/test-authentication.js
@@ -9,15 +9,15 @@ describe('Microsoft authentication', () => {
       let providerConfig = config('microsoft');
       auth.signin(providerConfig, {}, (err, data) => {
         expect(err).to.be.null;
-        expect(data.url).to.equal('https://login.live.com/oauth20_authorize.srf?client_id=fb-microsoft-id&redirect_uri=https://api-id.execute-api.eu-west-1.amazonaws.com/dev/callback/microsoft&scope=wl.basic&response_type=code');
+        expect(data.url).to.equal('https://login.live.com/oauth20_authorize.srf?client_id=fb-microsoft-id&redirect_uri=https://api-id.execute-api.eu-west-1.amazonaws.com/dev/callback/microsoft&response_type=code&scope=wl.basic');
       });
     });
 
     it('tests signin with scope and state params', () => {
-      let providerConfig = config('facebook');
+      let providerConfig = config('microsoft');
       auth.signin(providerConfig, {scope: 'wl.basic wl.emails', state: '123456'}, (err, data) => {
         expect(err).to.be.null;
-        expect(data.url).to.equal('https://login.live.com/oauth20_authorize.srf?client_id=undefined&redirect_uri=https://api-id.execute-api.eu-west-1.amazonaws.com/dev/callback/facebook&scope=wl.basic wl.emails&response_type=code&state=123456');
+        expect(data.url).to.equal('https://login.live.com/oauth20_authorize.srf?client_id=fb-microsoft-id&redirect_uri=https://api-id.execute-api.eu-west-1.amazonaws.com/dev/callback/microsoft&response_type=code&scope=wl.basic wl.emails&state=123456');
       });
     });
   });
