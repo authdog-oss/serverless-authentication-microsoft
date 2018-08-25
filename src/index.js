7c0928162b7c3abd75a36bcb91a273913ed3b509
Eetu Tuomala <laardee@outlook.com>
2018-08-26 00:32:48 +0300

    upgrade packages, use jest in tests, add prettier

--- src/index.js
+++ src/index.js
@@ -1,51 +1,52 @@
-'use strict';
-
-import { Profile, Provider } from 'serverless-authentication';
+import { Profile, Provider } from 'serverless-authentication'
 
 function mapProfile(response) {
   const overwrites = {
-    email: response.emails && response.emails.preferred ? response.emails.preferred : null,
+    email:
+      response.emails && response.emails.preferred
+        ? response.emails.preferred
+        : null,
     picture: `https://apis.live.net/v5.0/${response.id}/picture`,
     provider: 'microsoft'
-  };
+  }
 
-  return new Profile(Object.assign(response, overwrites));
+  return new Profile(Object.assign(response, overwrites))
 }
 
 class MicrosoftProvider extends Provider {
-  signinHandler({ scope = 'wl.basic', state }, callback) {
+  signinHandler({ scope = 'wl.basic', state }) {
     const options = Object.assign(
       { scope, state },
-      { signin_uri: 'https://login.live.com/oauth20_authorize.srf', response_type: 'code' }
-    );
+      {
+        signin_uri: 'https://login.live.com/oauth20_authorize.srf',
+        response_type: 'code'
+      }
+    )
 
-    super.signin(options, callback);
+    return super.signin(options)
   }
 
-  callbackHandler(event, callback) {
+  callbackHandler(event) {
     const options = {
       authorization_uri: 'https://login.live.com/oauth20_token.srf',
       profile_uri: 'https://apis.live.net/v5.0/me',
       profileMap: mapProfile,
       authorizationMethod: 'POST'
-    };
+    }
 
-    super.callback(
+    return super.callback(
       event,
       options,
-      { authorization: { grant_type: 'authorization_code' } },
-      callback
-    );
+      { authorization: { grant_type: 'authorization_code' } }
+    )
   }
 }
 
-const signinHandler = (config, options, callback) =>
-  (new MicrosoftProvider(config)).signinHandler(options, callback);
+const signinHandler = (config, options) =>
+  new MicrosoftProvider(config).signinHandler(options)
 
-const callbackHandler = (event, config, callback) =>
-  (new MicrosoftProvider(config)).callbackHandler(event, callback);
+const callbackHandler = (event, config) =>
+  new MicrosoftProvider(config).callbackHandler(event)
 
-exports.signinHandler = signinHandler;
-exports.signin = signinHandler; // old syntax, remove later
-exports.callbackHandler = callbackHandler;
-exports.callback = callbackHandler; // old syntax, remove later
+exports.signinHandler = signinHandler
+exports.callbackHandler = callbackHandler
